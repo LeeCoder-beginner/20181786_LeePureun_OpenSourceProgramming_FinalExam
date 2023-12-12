c7466aef2cd9d60985b8b0a579fdc2a68dd64c29
LeeCoder-beginner <76464923+LeeCoder-beginner@users.noreply.github.com>
2023-12-13 08:13:14 +0900

    Add files via upload

---
+++ Final_Exam/20181786_LeePureun_OpenSourceProgramming_FinalExam_Q.3.py
@@ -0,0 +1,24 @@
+# Q.3 10점
+#
+# 행성의 나이를 알파벳으로 표현할 때,
+# a는 0, b는 1, ..., j는 9
+# 예를 들어 cd는 23살, fb는 51살입니다.
+# age가 매개변수로 주어질 때
+# PROGEAMMER-857식 나이를 return 하도록
+# solution 함수를 완성하시오.
+#
+# 제한사항
+# age는 자연수입니다.
+# age ≤ 1,000
+# PROGRAMMERS-857 행성은 알파벳 소문자만 사용합니다.
+
+def solution(age):
+
+    answer = ''
+    for i in range(0, len(str(age))):
+      answer += chr(int(str(age)[i]) + 97)
+
+    return answer
+
+result = solution(857)
+print('PROGRAMMERS'.lower() + '-' + result)
